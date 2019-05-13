436adb9c0df7d847c06ac3ca23a5879df4d957cb
angelcf00 <49125784+angelcf00@users.noreply.github.com>
2019-05-13 23:13:09 +0200

    interfaces metidas

--- ProjectoFinalPracticas/src/application/Main.java
+++ ProjectoFinalPracticas/src/application/Main.java
@@ -1,25 +1,37 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			this.primaryStage = primaryStage;
+			this.primaryStage.setTitle("Componentes FX");
+
+			 // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("Menu.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+           } catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
